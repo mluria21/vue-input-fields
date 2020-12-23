42bcbc30e8c6bdb96fc4095d943a8cb989a0ed9e
Meilich Luria <meilichluria@Moshes-iMac.local>
2020-12-23 11:50:10 -0500

    wip

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
+import {terser} from 'rollup-plugin-terser';
+export default [{
+    input: 'src/forms/validators/index.js',
+    output: {
+      dir: 'dist/validators',
+      format: 'es',
+      plugins:[terser()]
+    },
+    plugins: [
+      babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
+      nodeResolve()
+    ],
+    external: [ /core-js/]
+  },
+  {
+    input: 'src/forms/formats/index.js',
+    output: {
+      dir: 'dist/formats',
+      format: 'es',
+      plugins:[terser()]
+    },
+    plugins: [
+      babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
+      nodeResolve()
+    ],
+    external: [ /core-js/]
+  },
+  {
+    input: 'src/forms/core/index.js',
+    output: {
+      dir: 'dist/core',
+      format: 'es',
+      plugins:[terser()]
+    },
+    plugins: [
+      babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
+      nodeResolve()
+    ],
+    external: [ /core-js/]
+  }];
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import {InputField} from './forms/core/input-field'
-
-let input = new InputField({
-    property:{name:'hello'}
-})
-
-console.log(input.value);

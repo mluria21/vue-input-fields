491e63e9ad5b3b64897c37986e533d2f4ba27ae9
meilech <mluria21@gmail.com>
2020-12-23 16:04:54 -0500

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,62 +1,42 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
-import {getBabelOutputPlugin} from '@rollup/plugin-babel'
 export default [{
     input: 'src/forms/validators/index.js',
     output: {
       dir: 'dist/validators',
       format: 'es',
-      plugins:[
-        getBabelOutputPlugin({
-          presets: ['@babel/preset-env'],
-          plugins: [
-            "@babel/plugin-proposal-class-properties"
-          ] },
-          terser())
-      ]
+      plugins:[terser()]
     },
     plugins: [
-      //babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
+      babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
       nodeResolve()
     ],
-    external: [ ]
+    external: ['vue',/core-js/]
   },
   {
     input: 'src/forms/formats/index.js',
     output: {
       dir: 'dist/formats',
       format: 'es',
-      plugins:[terser(),
-        getBabelOutputPlugin({
-          presets: ['@babel/preset-env'],
-          plugins: [
-            "@babel/plugin-proposal-class-properties"
-          ] })
-      ]
+      plugins:[terser()]
     },
     plugins: [
-      //babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
+      babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
       nodeResolve()
     ],
-    external: []
+    external: ['vue',/core-js/]
   },
   {
     input: 'src/forms/core/index.js',
     output: {
       dir: 'dist/core',
       format: 'es',
-      plugins: [
-        getBabelOutputPlugin({
-          presets: ['@babel/preset-env'],
-          plugins: [
-            "@babel/plugin-proposal-class-properties"
-          ] })
-      ],
+      plugins: [],
     },
     plugins: [
-      //babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
+      babel({ babelHelpers: 'bundled' ,exclude: 'node_modules/**',}),
       nodeResolve()
     ],
-    external: ['vue']
+    external: ['vue',/core-js/]
   }]; 

192ee287f809eca068e65a7889b20152f509a409
meilech <mluria21@gmail.com>
2020-12-24 11:51:20 -0500

    wip

---
+++ dist/validators/index.d.ts
@@ -0,0 +1,12 @@
+
+export function creditCard(value:string):string
+export function cvvCode(value:string,card:string):string
+export function email(value:string):string
+export function expirationDate(value:string):string
+export function getCreditCardType(value:string):string
+export function int(value:[string,number],label:string):string
+export function minLength(length:number,value:string,label:string):string
+export function phoneValid(value:string):string
+export function required(value:string,label:string):string
+export function sameAs(value:string,compare:[string,number],label:string):string
+export function usaZip(value:string):string
--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-import {required} from './dist/validators'
+import {int} from './dist/validators'

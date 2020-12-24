1c68a87365823a5af7001df838313fc16744f65b
meilech <mluria21@gmail.com>
2020-12-24 11:55:52 -0500

    wip

--- dist/validators/index.d.ts
+++ dist/validators/index.d.ts
@@ -1,12 +1,12 @@
 
-export function creditCard(value:string):string
-export function cvvCode(value:string,card:string):string
-export function email(value:string):string
-export function expirationDate(value:string):string
-export function getCreditCardType(value:string):string
-export function int(value:[string,number],label:string):string
-export function minLength(length:number,value:string,label:string):string
-export function phoneValid(value:string):string
-export function required(value:string,label:string):string
-export function sameAs(value:string,compare:[string,number],label:string):string
-export function usaZip(value:string):string
+export declare function creditCard(value:string):string
+export declare function cvvCode(value:string,card:string):string
+export declare function email(value:string):string
+export declare function expirationDate(value:string):string
+export declare function getCreditCardType(value:string):string
+export declare function int(value:[string,number],label:string):string
+export declare function minLength(length:number,value:string,label:string):string
+export declare function phoneValid(value:string):string
+export declare function required(value:string,label:string):string
+export declare function sameAs(value:string,compare:[string,number],label:string):string
+export declare function usaZip(value:string):string
